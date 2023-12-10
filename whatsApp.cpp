607202f9de781ad40603ff59f475766bae6f26ca
lal4lal <muhammadhilalaja@gmail.com>
2023-12-10 11:54:30 +0700

    2 createlist dan 3 create elm dan update adt

---
+++ whatsApp.cpp
@@ -0,0 +1,34 @@
+#include "whatsApp.h"
+
+void createListUser(listUser &LU) {
+    firstUser(LU) = NULL;
+}
+
+void createListGroup(listGroup &LG) {
+    firstGroup(LG) = NULL;
+}
+
+adrUser createElmUser(infotypeUser x) {
+    adrUser pUser = new elmUser;
+    infoUser(pUser) = x;
+    nextUser(pUser) = NULL;
+    firstRel(pUser) = NULL;
+
+    return pUser;
+}
+
+adrGroup createElmGroup(infotypeGroup x) {
+    adrGroup pGroup = new elmGroup;
+    infoGroup(pGroup) = x;
+    nextGroup(pGroup) = NULL;
+
+    return pGroup;
+}
+
+adrRel createElmRel(adrGroup pGroup) {
+    adrRel pRel = new elmRel;
+    nextGroup(pRel) = pGroup;
+    nextRel(pRel) = NULL;
+
+    return pRel;
+}
